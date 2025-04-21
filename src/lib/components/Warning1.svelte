f53e10b7c98139afd4049d7f9fcc9ad4a0377b2b
ardhas20 <ardhas20@htl-shkoder.com>
2025-04-21 21:01:33 +0200

    added admin page

---
+++ src/lib/components/Warning1.svelte
@@ -0,0 +1,9 @@
+<script>
+    const { message } = $props();
+</script>
+
+<div>
+    <p class="bg-red-500">
+        {message}
+    </p>
+</div>
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { enhance } from '$app/forms';
+	import Warning from '$lib/components/Warning1.svelte';
+	import { slide } from 'svelte/transition';
+	const { data } = $props();
+</script>
+
+
+<h1>Admin dashboard</h1>
+
+
+<div class="container">
+	{#each data.articles as article (article.id)}
+		<div class="box" transition:slide>
+			<p>ID: {article.id} - Description: {article.description} - Author: {article.author} - Votes: {article.votes}</p>
+			<form action="?/deleteArticle" method="POST" use:enhance>
+				<input type="hidden" name="id" value={article.id} />
+				<button type="submit">Delete</button>
+			</form>
+		</div>
+	{/each}
+</div>
